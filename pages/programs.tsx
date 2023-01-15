731755f82e526878e48b138a883233f749293ef1
Acheron_Fox <captain.fox.2k@gmail.com>
2023-01-15 17:49:42 +0100

    Started Home Screen, added prices

--- pages/programs.tsx
+++ pages/programs.tsx
@@ -1,9 +1,9 @@
 import styles from "@/styles/pages/Home.module.scss"
 
-export default function Orli() {
+export default function Programs() {
   return (
     <div className={styles.Home}>
-      Orli
+      Programs
     </div>
   )
 }
