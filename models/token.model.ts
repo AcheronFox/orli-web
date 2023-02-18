6b0eb05a214ed7d90d3de37eebf088c63095a68c
AcheronFox <captain.fox.2k@gmai.com>
2023-02-18 13:30:40 +0100

    Deploymnt

--- models/token.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IToken {
-    iss: string
-    iat: number
-    exp: number
-    aud: string
-    accountKey: string
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,10 @@
-import PrimaryButton from "@/comp/PrimaryButton"
-import styles from "@/styles/pages/Home.module.scss"
-import { RiArrowDownSLine } from "react-icons/ri";
-import { useTranslate } from "@/hooks/useTranslate";
 import { NextPage } from "next";
 
 type Props = {}
 
 const Home: NextPage<Props> = (props: Props) => {
-  const { t } = useTranslate();
-
   return (
-    <div>
-      <section className={styles.Home}>
-        <div className={styles.Home__TextField}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis facilis nobis dignissimos obcaecati voluptatem ad impedit ducimus, at voluptates? Id culpa itaque repellendus numquam magnam, odio est recusandae deserunt sapiente?
-          <div className={styles.Home__TextField__Content}>
-            {
-            /*
-              <iframe
-                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-              </iframe>
-            */
-            }
-          </div>
-          <div className={styles.Home__TextField__Btn}>
-            <PrimaryButton text={t("homeReg")} link="/registration"></PrimaryButton>
-          </div>
-        </div>
-      </section>
-    </div>
+    <></>
   )
 }
 
