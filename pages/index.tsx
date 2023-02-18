23153c24a28ed0d635836e54753ec44d8888ce03
AcheronFox <captain.fox.2k@gmai.com>
2023-02-18 22:03:08 +0100

    Deployment test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import { NextPage } from "next";
+import styles from "@/styles/pages/Home.module.scss"
+import SocialMediaButton from "@/comp/SocialMediaButton";
+import { RiTelegramLine, RiNotification3Line, RiDiscussLine } from 'react-icons/ri';
 
 type Props = {}
 
 const Home: NextPage<Props> = (props: Props) => {
   return (
-    <></>
+    <div className={styles.Home}>
+      <div className={styles.Home__Item}>
+        <h1>
+          Chat With Us: 
+        </h1>
+        <SocialMediaButton link={"https://t.me/orliforstivalHU"} icon={<RiTelegramLine />} icon2={<RiDiscussLine />} label="Telegram Chat" />
+      </div>
+      <div className={styles.Home__Item}>
+        <h1>
+          Get The Latest Updates:
+        </h1>
+        <SocialMediaButton link={"https://t.me/orliforsztival"} icon={<RiTelegramLine />} icon2={<RiNotification3Line />} label="Telegram News Channel" />
+      </div>
+    </div>
   )
 }
 
