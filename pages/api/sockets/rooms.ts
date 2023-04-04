fbd811bdacb8d91698c820ceb1d8f1265c5b04e9
Acheron_Fox <captain.fox.2k@gmail.com>
2023-04-05 00:02:44 +0200

    websocket yaaay

--- pages/api/sockets/rooms.ts
+++ pages/api/sockets/rooms.ts
@@ -2,11 +2,22 @@ const { Server } = require("socket.io");
 
 const SocketHandler = (req: any, res: any) => {
   if (res.socket.server.io) {
-    console.log('Socket is already running')
+    // Existing connection
+    res.socket.server.io.on('connection', (socket: any) => {
+        socket.on('room-change', () => {
+            socket.broadcast.emit('update-room')
+        })
+      })
   } else {
-    console.log('Socket is initializing')
+    // New connection
     const io = new Server(res.socket.server)
     res.socket.server.io = io
+
+    io.on('connection', (socket: any) => {
+        socket.on('room-change', () => {
+            socket.broadcast.emit('update-room')
+        })
+    })
   }
   res.end()
 }
