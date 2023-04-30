69a1ab8bd76632a3b911cc366beebf2db42ee312
Acheron_Fox <captain.fox.2k@gmail.com>
2023-04-30 20:17:42 +0200

    a

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -654,18 +654,11 @@ const Registration: NextPage<Props> = (props: Props) => {
                 ></Input>
             </div>
             {
-              (serverDate != undefined && fromDate != undefined && toDate != undefined) &&
               <div className={styles.Registration__Button}>
                 <>
-                  {
-                    (!((serverDate.getTime() > fromDate.getTime()) && (serverDate.getTime() < toDate.getTime()))) &&
-                    <p style={{color: 'red'}}>
-                      {`${t("warnDateLimit1")} ${createDatePatternFromDate(fromDate)} - ${createDatePatternFromDate(toDate)} ${t("warnDateLimit2")}`}
-                    </p>
-                  }
+                  
                   <PrimaryButton
-                  disabled={isButtonActive || isDisabled ||
-                    !((serverDate.getTime() > fromDate.getTime()) && (serverDate.getTime() < toDate.getTime()))}
+                  disabled={isButtonActive || isDisabled}
                   text={t("regButton")}
                   onClick={handleButton} 
                   />
