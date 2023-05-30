fe3fbb0ecb1cf0e088b243dc58f6b2b2384631ed
Acheron_Fox <captain.fox.2k@gmail.com>
2023-05-30 20:24:20 +0200

    Removed console log

--- pages/profile/tickets.tsx
+++ pages/profile/tickets.tsx
@@ -329,7 +329,6 @@ const Tickets: NextPage<Props> = (props: Props) => {
   }, [selectedFoods, selectedDayIndex, selectedDate, selectedTicket, wantsDay0, wantsDayExtra, shirtSize])
 
   const evalAmountOfDays = () => {
-    console.log(selectedDate)
     if (selectedDate.length < 2 || selectedTicket == 2 || (selectedTicket == 1 && selectedDate.length != 2)) return 0
     else {
       const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
