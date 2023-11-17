03fdaf0e0b4f9104bccf34b29b290e99480d24da
Acheron_Fox <captain.fox.2k@gmail.com>
2023-11-17 12:43:52 +0100

    a

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer: NextPage<Props> = (props: Props) => {
                 </div>
             </div>
             {
-                false &&
+                true &&
                 <div className={styles.Footer__Item}>
                     <div>
                         <div className={styles.Footer__Item__Title}>
