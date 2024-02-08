d30ffc72f5675476e0ed685b16b60f7935923425
Tib Averus <tibaverus@predationgame.com>
2024-02-08 02:31:26 +0100

    [Db] Prisma client generated
    -Modified the database one last time
    -Created a migration from the new database
    -Generated the client for Prisma

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,5 +1,3 @@
-yarn run v1.22.21
-$ C:\Users\tib\Documents\GitHub\orli-web\node_modules\.bin\prisma migrate diff --from-empty --to-schema-datamodel .\prisma\schema.prisma --script
 -- CreateTable
 CREATE TABLE `accomodation` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -32,14 +30,25 @@ CREATE TABLE `attendee` (
     `accomodationId` INTEGER NULL,
     `passwordResetTokenId` INTEGER NULL,
     `ticketId` INTEGER NULL,
+    `dailyTicketId` INTEGER NULL,
 
     UNIQUE INDEX `fursonaId_UNIQUE`(`fursonaId`),
     INDEX `fk_attendee_accomodation1_idx`(`accomodationId`),
+    INDEX `fk_attendee_dailyTicket1_idx`(`dailyTicketId`),
     INDEX `fk_attendee_fursona_idx`(`fursonaId`),
     INDEX `fk_attendee_nationality1_idx`(`nationalityId`),
     INDEX `fk_attendee_passwordResetToken1_idx`(`passwordResetTokenId`),
     INDEX `fk_attendee_ticket1_idx`(`ticketId`),
     PRIMARY KEY (`id`, `nationalityId`, `fursonaId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `dailyTicket` (
+    `id` INTEGER NOT NULL,
+    `currentValidity` DATE NOT NULL,
+    `pastValidities` VARCHAR(512) NULL,
+
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -70,7 +79,7 @@ CREATE TABLE `nationality` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `passwordresettoken` (
+CREATE TABLE `passwordResetToken` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `token` VARCHAR(255) NOT NULL,
     `tokenExpireTime` INTEGER NOT NULL,
@@ -81,7 +90,7 @@ CREATE TABLE `passwordresettoken` (
 -- CreateTable
 CREATE TABLE `room` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `building` ENUM('F├íc├ín', 'Vidra', 'K├│csag', '') NOT NULL,
+    `building` ENUM('Facan', 'Vidra', 'Kocsag') NOT NULL,
     `number` VARCHAR(15) NOT NULL,
     `size` INTEGER NOT NULL,
     `customName` VARCHAR(20) NULL,
@@ -100,9 +109,9 @@ CREATE TABLE `ticket` (
     `shirtSize` ENUM('S', 'M', 'L', 'XL', 'XXL', 'XXXL') NULL,
     `sponsorPrice` INTEGER NOT NULL,
     `totalPrice` INTEGER NOT NULL,
-    `paymentMethod` ENUM('MKB', 'PayPal', 'Revolut', '') NULL,
+    `paymentMethod` ENUM('MKB', 'PayPal', 'Revolut') NULL,
     `isPaid` TINYINT NOT NULL DEFAULT 0,
-    `foodData` JSON NULL,
+    `foodData` LONGTEXT NULL,
     `arrivalDate` DATE NOT NULL,
     `departureDate` DATE NOT NULL,
     `createdAt` TIMESTAMP(0) NULL,
@@ -117,15 +126,16 @@ ALTER TABLE `accomodation` ADD CONSTRAINT `fk_accomodation_room1` FOREIGN KEY (`
 ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_accomodation1` FOREIGN KEY (`accomodationId`) REFERENCES `accomodation`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
+ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_dailyTicket1` FOREIGN KEY (`dailyTicketId`) REFERENCES `dailyTicket`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
 ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_fursona` FOREIGN KEY (`fursonaId`) REFERENCES `fursona`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_nationality1` FOREIGN KEY (`nationalityId`) REFERENCES `nationality`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_passwordResetToken1` FOREIGN KEY (`passwordResetTokenId`) REFERENCES `passwordresettoken`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_passwordResetToken1` FOREIGN KEY (`passwordResetTokenId`) REFERENCES `passwordResetToken`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `attendee` ADD CONSTRAINT `fk_attendee_ticket1` FOREIGN KEY (`ticketId`) REFERENCES `ticket`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-Done in 0.50s.
