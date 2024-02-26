8c426faa868d02f019eba84e4352e2860b88b89f
Acheron_Fox <captain.fox.2k@gmail.com>
2024-02-26 22:24:47 +0100

    Transformed staff and prices page

---
+++ locales/en/en.staff.ts
@@ -0,0 +1,93 @@
+import { IStaff } from "@/models/locale/staff.model";
+
+const staff: IStaff[] = [
+    {
+        name: "Maszat",
+        description: [
+            "I had a thought that we should have a multi-day summer festival at home for the furry audience.<br/>",
+            "I am working on what is necessary for this and helping with the organization so that everyone can have a good time at the event!",
+        ],
+        picture: "staff/maszat.jpg"
+    },
+    {
+        name: "Freddie",
+        description: [
+            "Maszat's right hand.",
+            "Jack-of-all-trades. Fox."
+        ],
+        picture: "staff/freddie.jpg"
+    },
+    {
+        name: "Acheron",
+        description: [
+            `
+                <ul>
+                    <li>Translation</li>
+                    <li>Web development</li>
+                    <li>Server owner and administrator</li>
+                    <li>IT all-rounder</li>
+                </ul>
+            `
+        ],
+        picture: "staff/acheron.jpg"
+    },
+    {
+        name: "Helios",
+        description: [
+            `
+                <ul>
+                    <li>Translation</li>
+                    <li>Interpretation</li>
+                    <li>Copywriting</li>
+                    <li>Proofreading</li>
+                    <li>On-site and other tasks</li>
+                </ul>
+            `
+        ],
+        picture: "staff/helios.jpg"
+    },
+    {
+        name: "Gali",
+        description: [
+            `
+                <ul>
+                    <li>Sound and lighting technician</li>
+                    <li>The host of NicoBar</li>
+                </ul>
+            `
+        ],
+        picture: "staff/gali.jpg"
+    },
+    {
+        name: "Bethlenke",
+        description: [
+            "Hello everyone! I am Bethlenke.<br/>",
+            "I am one of the team members responsible for the sound and technical aspects of the event to ensure the best atmosphere at the party. :D",
+        ],
+        picture: "staff/bethlenke.jpg"
+    },
+    {
+        name: "Northern",
+        description: [
+            "Hello everyone!<br/>",
+            "I wlil make the event colorful and bright.<br/>",
+            "My job is to implement and operate mood lights at the event, handle the electronic part of the suit dryers, and prepare weather forecasts for the duration of the event.",
+        ],
+        picture: "staff/northern.jpg"
+    },
+    {
+        name: "Naya",
+        description: [
+            `
+                <ul>
+                    <li>Responsible for print submissions</li>
+                    <li>Assistance with graphic tasks</li>
+                    <li>On-site and other tasks</li>
+                </ul>
+            `
+        ],
+        picture: "staff/naya.jpg"
+    }
+]
+
+export default staff
