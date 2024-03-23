027b7eee4fea0d51f1640d411b53bc92cdb0b520
Tib Averus <tibaverus@predationgame.com>
2024-03-23 22:02:19 +0100

    [Db] Almost finished with Attendee services
    -Added almost every function for dealing with entries from the Attendee table
    -Attendee deleting needs work once the other services are finished!

---
+++ services/attendee/service.attendee.update.ts
@@ -0,0 +1,119 @@
+import { executeUpdateQuery } from "@/functions/utils/databaseHelpers";
+import { IAttendee } from "@/models/newDbModels/attendee.model";
+import { IAttendeeUpdatable } from "@/models/newDbModels/updateModels/updatable.attendee.model";
+import * as console from "console";
+
+const TABLE: string = "attendee";
+
+export async function modifyAttendee(attendee: IAttendee) : Promise<number>
+{
+    let editable: IAttendeeUpdatable = {
+        accountKey: attendee.accountKey,
+        firstName: attendee.firstName,
+        lastName: attendee.lastName,
+        email: attendee.email,
+        password: attendee.password,
+        dateOfBirth: attendee.dateOfBirth,
+        phone: attendee.phone,
+        telegram: attendee.telegram,
+        allergy: attendee.allergy,
+    }
+
+    const query: string = `UPDATE ${TABLE} SET ? WHERE id = ?;`;
+
+    return await executeUpdateQuery(query, [editable, attendee.id]);
+}
+
+export async function changeAttendeeVerification(attendee: IAttendee, verified: boolean) : Promise<boolean>;
+export async function changeAttendeeVerification(attendeeId: number, verified: boolean) : Promise<boolean>;
+export async function changeAttendeeVerification(arg1: IAttendee | number, verified: boolean) : Promise<boolean>
+{
+    const query: string = `UPDATE ${TABLE} SET verified = ? WHERE id = ?;`;
+
+    const id: number | undefined = typeof arg1 == 'number' ? arg1 : arg1.id;
+
+    if (id == undefined)
+    {
+        console.error("ID cannot be undefined!")
+        return false;
+    }
+
+    const result: number = await executeUpdateQuery(query, [verified, id]);
+
+    return result >= 1;
+}
+
+export async function changeAttendeePassword(attendee: IAttendee, newHash: string, newAccountKey: string)
+    : Promise<boolean>;
+export async function changeAttendeePassword(attendeeId: number, newHash: string, newAccountKey: string)
+    : Promise<boolean>;
+export async function changeAttendeePassword(arg1: IAttendee | number, newHash: string, newAccountKey: string)
+    : Promise<boolean>
+{
+    const query: string = `UPDATE ${TABLE} SET password = ?, accountKey = ? WHERE id = ?;`;
+
+    const id: number | undefined = typeof arg1 == 'number' ? arg1 : arg1.id;
+    if (id == undefined)
+    {
+        console.error("ID cannot be undefined!")
+        return false;
+    }
+
+    const result: number = await executeUpdateQuery(query, [newHash, newAccountKey, id]);
+
+    return result >= 1;
+}
+
+export async function changeAttendeeStaffStatus(attendee: IAttendee, staffStatus: boolean) : Promise<boolean>;
+export async function changeAttendeeStaffStatus(attendeeId: number, staffStatus: boolean) : Promise<boolean>;
+export async function changeAttendeeStaffStatus(arg1: IAttendee | number, staffStatus: boolean) : Promise<boolean>
+{
+    const query: string = `UPDATE ${TABLE} SET staff = ? WHERE id = ?;`;
+    const id: number | undefined = typeof arg1 == 'number' ? arg1 : arg1.id;
+
+    if (id == undefined)
+    {
+        console.error("ID cannot be undefined!")
+        return false;
+    }
+
+    const result: number = await executeUpdateQuery(query, [staffStatus, id]);
+
+    return result >= 1;
+}
+
+export async function changeAttendeeAdminStatus(attendee: IAttendee, adminStatus: boolean) : Promise<boolean>;
+export async function changeAttendeeAdminStatus(attendeeId: number, adminStatus: boolean) : Promise<boolean>;
+export async function changeAttendeeAdminStatus(arg1: IAttendee | number, adminStatus: boolean) : Promise<boolean>
+{
+    const query: string = `UPDATE ${TABLE} SET admin = ? WHERE id = ?;`;
+    const id: number | undefined = typeof arg1 == 'number' ? arg1 : arg1.id;
+
+    if (id == undefined)
+    {
+        console.error("ID cannot be undefined!")
+        return false;
+    }
+
+    const result: number = await executeUpdateQuery(query, [adminStatus, id]);
+
+    return result >= 1;
+}
+
+export async function changeAttendeePasswordResetTokenId(attendee: IAttendee, tokenId: number) : Promise<boolean>;
+export async function changeAttendeePasswordResetTokenId(attendeeId: number, tokenId: number) : Promise<boolean>;
+export async function changeAttendeePasswordResetTokenId(arg1: IAttendee | number, tokenId: number) : Promise<boolean>
+{
+    const query: string = `UPDATE ${TABLE} SET passwordResetTokenId = ? WHERE id = ?;`;
+    const id: number | undefined = typeof arg1 == 'number' ? arg1 : arg1.id;
+
+    if (id == undefined)
+    {
+        console.error("ID cannot be undefined!")
+        return false;
+    }
+
+    const result: number = await executeUpdateQuery(query, [tokenId, id]);
+
+    return result >= 1;
+}
