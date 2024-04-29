97e40d057014427bf8a847fa28eac9addfdca07e
Tib Averus <tibaverus@predationgame.com>
2024-04-29 14:18:45 +0200

    [Api] room/leave now uses transactions

--- pages/api/room/leave.ts
+++ pages/api/room/leave.ts
@@ -9,6 +9,7 @@ import _ from 'lodash';
 import { getAccomodationById, getAccomodationsByRoomId } from '@/services/accomodation/service.accomodation.select';
 import { getAttendeeByAccountKey, getAttendeeById } from '@/services/attendee/service.attendee.select';
 import { leaveRoom } from '@/services/accomodation/service.accomodation.update';
+import { beginDbTransaction, getDbConnection } from '@/functions/utils/databaseHelpers';
 
 
 export default async function handler(
@@ -29,38 +30,66 @@ export default async function handler(
         return;
 
     if (!req.body.roomId || !req.body.roomCount) {
-        return sendResponse(400, {message: "Missing prop", e_code: "room_leave_2"})
+        return sendResponse(400, {message: "Missing prop", e_code: "room_leave_1"})
     }
 
-    const occupants = await getAccomodationsByRoomId(req.body.roomId);
-    const attendee = await getAttendeeByAccountKey(tokenPayload.accountKey);
-    if (attendee == undefined) {
-        return sendResponse(400, { message: "Attendee with account key not found", e_code: "room_leave_10"});
+    let connection: mysql.PoolConnection | null = null;;
+
+    try {
+        connection = await getDbConnection();
+        await beginDbTransaction(connection);
+
+        const occupants = await getAccomodationsByRoomId(req.body.roomId, connection);
+        const attendee = await getAttendeeByAccountKey(tokenPayload.accountKey, connection);
+        if (attendee == undefined) {
+            throw new DatabaseError(400, "Attendee with account key not found", "room_leave_2")
+        }
+    
+        if (!occupants) {
+            throw new DatabaseError(400, "No attendees in room", "room_leave_3");
+        }
+    
+        if (occupants.length != req.body.roomCount) {
+            throw new DatabaseError(409, "Data changed", "room_leave_4");
+        }
+        if (!occupants.find((o) => o.id == attendee.accomodationId)) {
+            throw new DatabaseError(400, "User not in room", "room_leave_5");
+        }
+    
+        if (attendee.accomodationId == undefined) {
+            throw new DatabaseError(400, "Attendee has no accomodation", "room_leave_6");
+        }
+    
+        const accomodation = await getAccomodationById(attendee.accomodationId, connection);
+    
+        if (accomodation == undefined) {
+            throw new DatabaseError(400, "Can't find accomodation for attendee", "room_leave_7");
+        }
+    
+        const result = await leaveRoom(accomodation, connection);
+    
+        if (result)
+            sendResponse(201, {message: "Room left"});
+
+    } catch (err) {
+        if (connection) {
+            await new Promise<void>(resolve => connection!.rollback(() => {
+                connection!.release();
+                resolve();
+            }));
+        }
+
+        if (err instanceof DatabaseError) {
+            sendResponse(err.return_code, { message: err.message, e_code: err.e_code });
+        } else {
+            sendResponse(500, { message: `Unknown error occured: ${err}`, e_code: "room_leave_8" });
+        }
+    } finally {
+        if (connection) {
+            connection.release();
+        }
     }
 
-    if (!occupants) {
-        return sendResponse(400, { message: "No attendees in room", e_code: "room_leave_11"});
-    }
-
-    if (occupants.length != req.body.roomCount) {
-        return sendResponse(409, {message: "Data changed", e_code: "room_leave_3"})
-    }
-    if (!occupants.find((o) => o.id == attendee.accomodationId)) {
-        return sendResponse(400, {message: "User not in room", e_code: "room_leave_4"})
-    }
-
-    if (attendee.accomodationId == undefined) {
-        return sendResponse(400, {message: "Attendee has no accomodation", e_code: "room_leave_12"});
-    }
-
-    const accomodation = await getAccomodationById(attendee.accomodationId);
-
-    if (accomodation == undefined) {
-        return sendResponse(400, { message: "Can't find accomodation for attendee", e_code: "room_leave_13"});
-    }
-
-    const result = await leaveRoom(accomodation);
-
-    if (result)
-        sendResponse(201, {message: "Room left"});
+    // We should not get here at all.
+    sendResponse(500, { message: "Unknown error", e_code: "room_leave_9" });
 }
