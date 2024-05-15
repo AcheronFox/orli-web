c51f67e7c2b96bcfcc479de03daf175a79d14957
Acheron_Fox <captain.fox.2k@gmail.com>
2024-05-15 14:39:03 +0200

    Adjusted config

--- private/app.config.ts
+++ private/app.config.ts
@@ -2,7 +2,7 @@ import {IAppConfig} from "@/models/app.config.model";
 
 const configuration: IAppConfig = {
     profile: {
-        uploadToDate: new Date(2024, 5, 2)
+        uploadToDate: new Date(2024, 5, 6)
     },
     registration: {
         start: new Date(2024, 0, 1), // LIVE -> new Date(2024, 4, 15, 20, 0, 0)
@@ -18,13 +18,13 @@ const configuration: IAppConfig = {
                 name: "WACC",
                 price: "68 000",
                 earlyBirdPrice: "58 000",
-                limit: 88
+                limit: 88,
             },
             {
                 name: "TENT",
                 price: "48 000",
                 earlyBirdPrice: "40 000",
-                limit: 80
+                limit: 80,
             },
             {
                 name: "NACC",
@@ -35,13 +35,13 @@ const configuration: IAppConfig = {
                 name: "EARLY",
                 price: "7 000",
                 earlyBirdPrice: "6 000",
-                limit: 168
+                limit: 168,
             },
             {
                 name: "LATE",
                 price: "8 000",
                 earlyBirdPrice: "7 000",
-                limit: 32
+                limit: 32,
             },
             {
                 name: "DAILY",
@@ -54,7 +54,7 @@ const configuration: IAppConfig = {
             {
                 name: "SSPONS",
                 price: "12 001",
-            }
+            },
         ]
     }
 }
